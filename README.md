85552e1297acb1c61f5fd34d6412dec3645c67ca
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-10-25 12:29:41 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dining Philosophers
+
+### Problem
+<p align="justify">In ancient times, a wealthy philanthropist set up a university to accommodate five eminent philosophers. Each philosopher had a room in which they could carry out their professional activity of thinking. There was also a common dining room furnished with a circular table surrounded by five chairs, each one identified with the name of the philosopher who sat in it. The philosophers sat counterclockwise around the table. To the left of each philosopher lay a golden fork, and in the center, a bowl of spaghetti, which was constantly refilled. It was expected that a philosopher would spend most of their time thinking. However, when they felt hungry, they would go to the dining room, take the fork to their left and immerse it in the spaghetti. But the spaghetti was so tangled in nature that a second fork was required to bring it to the mouth. The philosopher, therefore, had to also take the fork to their right. When they finished, they had to put down both forks, get up from the chair, and resume thinking. Of course, a fork can be used by only one philosopher at a time. If another philosopher wishes to use it, they must wait until the fork is available again.</p>
+
+<p align="justify">Only two philosophers can eat at any given time and consequently the output will look like this:</p>
+
+
+    Gilles Deleuze esta comiendo.
+    Emma Goldman esta comiendo.
+    Emma Goldman ha finalizado de comer.
+    Gilles Deleuze ha finalizado de comer.
+    Judith Butler esta comiendo.
+    Karl Marx esta comiendo.
+    Judith Butler ha finalizado de comer.
+    Michel Foucault esta comiendo.
+    Karl Marx ha finalizado de comer.
+    Michel Foucault ha finalizado de comer.
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::sync::Arc;
+use tokio::{
+    sync::{Barrier, Notify},
+    time::{sleep, Duration},
+};
+
+async fn philosopher_eating(name: &str, barrier: Arc<Barrier>, notify: Arc<Notify>) -> () {
+    println!("{} está comiendo.", name);
+    let wait_result = barrier.wait().await;
+    println!("{} ha finalizado de comer.", name);
+
+    if wait_result.is_leader() {
+        notify.notify_one();
+    }
+}
+#[tokio::main]
+async fn main() {
+    let total_philosopher_eating = 2;
+
+    let barrier = Arc::new(Barrier::new(total_philosopher_eating));
+    let barrier_final = Arc::new(Barrier::new(1));
+    let notify = Arc::new(Notify::new());
+    notify.notify_one();
+
+    let philosophers: Vec<&str> = vec![
+        "Gilles Deleuze",
+        "Emma Goldman",
+        "Judith Butler",
+        "Karl Marx",
+        "Michel Foucault",
+    ];
+
+    let num_philosophers = philosophers.len();
+
+    let mut sorted_philosophers = philosophers.clone();
+    sorted_philosophers.sort(); // Ordenar alfabéticamente
+
+    let handle_spawn = sorted_philosophers.into_iter().enumerate().fold(
+        Vec::new(),
+        |mut handles, (i, philosopher)| {
+            let philosopher_arc = philosopher;
+            let barrier_arc;
+            if i == num_philosophers - 1 {
+                barrier_arc = barrier_final.clone();
+            } else {
+                barrier_arc = barrier.clone();
+            }
+            let notify_arc = notify.clone();
+
+            let handle = tokio::spawn(async move {
+                if i % total_philosopher_eating == 0 {
+                    notify_arc.notified().await;
+                    sleep(Duration::from_secs(2)).await;
+                }
+                philosopher_eating(philosopher_arc, barrier_arc, notify_arc).await;
+            });
+            handles.push(handle);
+            handles
+        },
+    );
+    for handle in handle_spawn {
+        let _result = handle.await.unwrap();
+    }
+}
